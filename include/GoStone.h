9927f7c73001384eaf5d788023122b17a9d26116
eopXD <yueh.ting.chen@gmail.com>
2019-12-04 22:18:20 +0800

    adjust comments

--- include/GoStone.h
+++ include/GoStone.h
@@ -1,7 +1,9 @@
 // Copyright (C) 2019 Yueh-Ting Chen (eopXD)
 /*! \file GoStone.h
-	\brief Do disjoint-set on the stones, also record the block_id of which 
-	 the stone corresponds to, this can increase speed of move generation.
+	\brief Circular linked-list to connnect stones.
+	 Disjoint set to find the ancestor stone of the block.
+	 Each stone contains its own id, and holds a GoBlockId
+	 that represents the GoBlock that contains the stone.
 
 	\author Yueh-Ting Chen (eopXD)
 	\project Efficient & Succinct Small Board Go
@@ -15,7 +17,7 @@
 struct GoStone {
 	GoCoordId self_id;      // Id of this stone
 	GoBlockId block_id;     // Id of its block
-	GoCoordId next_id;      // Use like link-list
+	GoCoordId next_id;      // one-way linked-list to connect stones
 	GoCoordId parent_id;    // Use like union-find-set (rooted by tail)
 
 	// inline void SetSelfId(GoCoordId id) { self_id = id; }
@@ -23,6 +25,7 @@ struct GoStone {
 // this function mainly used when... GoBoard::Move, 
 // set the stone to the blk_id
 	inline void Reset ( GoBlockId blk_id=GoConstant::BLOCK_UNSET ) {
+// self_id shall be set initially when we allocate the stone
 		next_id = parent_id = self_id;
 		block_id = blk_id;
 	}
